fef5057e8870656dc8bd0ba9bf11352eaa3e9c72
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-06 22:49:07 +0200

    créer des noeuds à gauche

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - insert des noeuds à gauche
+ * @parent: la racine de l'arbre
+ * @value: la valeur du noeud
+ *
+ * Return: (new_node_L) renvoie le noeud créé, (NULL) si adresse vide
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node_L;
+	binary_tree_t *old_node;
+
+	if (!parent)
+		return (NULL);
+
+	if (parent && !parent->left)
+	{
+		new_node_L = binary_tree_node(parent, value);
+		parent->left = new_node_L;
+		return (new_node_L);
+	}
+	else if (parent && parent->left)
+	{
+		old_node = parent->left;
+		new_node_L = binary_tree_node(parent, value);
+		new_node_L->left = old_node;
+		parent->left = new_node_L;
+		old_node->parent = new_node_L;
+		return (new_node_L);
+	}
+return (NULL);
+}
