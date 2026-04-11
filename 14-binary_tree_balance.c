c11d4e8769a668255fec9693102c32c160d4e3a4
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-11 15:16:27 +0200

    balance d'un arbre

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_height - hauteur d'un arbre
+ * @tree: pointeur sur une struture pour abre
+ *
+ * Return: (left_height + 1) pour retourner le valeur de la taille
+ * des arretes gauche, (left_height + 1) celle des arretes droite
+ * (0) si échec.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
+}
+/**
+ * binary_tree_balance - détermine la valeur de la balance
+ * @tree: pointeur sur structure d'un arbre
+ *
+ * Return: (), (0)si échec
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left_height = binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = binary_tree_height(tree->right);
+
+return ((int)(left_height - right_height));
+}
