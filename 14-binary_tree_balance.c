b083f3709b652388dd007db2afb4b176e96d712a
Jonathan <jonathan.cahoreau@gmail.com>
2026-04-11 15:41:07 +0200

    balance d'un arbre

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,11 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		left_height = 1 + binary_tree_height(tree->left);
-
-	if (tree->right)
-		right_height = 1 + binary_tree_height(tree->right);
+	left_height = 1 + binary_tree_height(tree->left);
+	right_height = 1 + binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height);
@@ -35,18 +32,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-
-	size_t left_height = 0;
-	size_t right_height = 0;
-
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		left_height = binary_tree_height(tree->left);
-
-	if (tree->right)
-		right_height = binary_tree_height(tree->right);
-
-return ((int)(left_height - right_height));
+return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
